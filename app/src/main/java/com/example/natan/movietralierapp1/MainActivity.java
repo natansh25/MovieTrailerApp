20e09de2ed1f521df5124169f5792d504b2f4f7d
Natansh <natansh25@gmail.com>
2017-11-19 19:34:55 -0500

    json response fetched

--- app/src/main/java/com/example/natan/movietralierapp1/MainActivity.java
+++ app/src/main/java/com/example/natan/movietralierapp1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.natan.movietralierapp1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -9,17 +11,51 @@ import android.widget.Toast;
 
 import com.example.natan.movietralierapp1.Network.NetworkUtils;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txt1;
+    TextView txt1,txtjson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt1=findViewById(R.id.txt);
+        txtjson=findViewById(R.id.json);
     }
+
+
+    //Creating inner class for Async Task
+
+    public class MovieDbQUeryTask extends AsyncTask<URL,Void,String>{
+
+
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL searchUrl=urls[0];
+            String MovieDbResults=null;
+            try{
+                MovieDbResults=NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return MovieDbResults;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            txtjson.setText(s.toString());
+            Log.i("msg",s);
+        }
+    }
+
+
 
 
     // For menu settings
@@ -38,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.higest_rated:
                 Toast.makeText(this, "Highest Rated", Toast.LENGTH_SHORT).show();
                 URL sortUrl= NetworkUtils.buildURl("vote_count.desc");
+                Log.i("url",sortUrl.toString());
                 txt1.setText(sortUrl.toString());
+                new MovieDbQUeryTask().execute(sortUrl);
 
 
 
@@ -48,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "most popular", Toast.LENGTH_SHORT).show();
                 URL sortUrl2= NetworkUtils.buildURl("popularity.desc");
                 txt1.setText(sortUrl2.toString());
+                new MovieDbQUeryTask().execute(sortUrl2);
                 break;
         }
 
